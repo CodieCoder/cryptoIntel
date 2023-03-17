16dbb4ffebf5ebee3efd170c38a1088ccbf37d6f
Boss <nonso4emma@yahoo.com>
2023-03-17 08:06:14 +0200

    trest

--- src/pages/Homepage/Body/CenterBody/index.tsx
+++ src/pages/Homepage/Body/CenterBody/index.tsx
@@ -3,6 +3,7 @@ import CoinsListing from "../../../../components/CoinList.tsx"
 import "./index.scss"
 import TopStories from "./headlines"
 
+// centerbody
 const CenterBody = () => {
   return (
     <div className="center-body">
