10091d739a780b9a71b411ddaf23187cb66fe291
Boss <nonso4emma@yahoo.com>
2023-05-02 00:12:15 +0300

    Added loading placeholder for news

--- src/pages/News/AllNews/index.tsx
+++ src/pages/News/AllNews/index.tsx
@@ -7,9 +7,9 @@ import { Row, Col } from "react-bootstrap"
 import LandscapeCards from "components/News/LandsscapeNewsCard/landscapeCard"
 import NewsBrief from "Widgets/Briefs"
 import { TNews } from "Constants"
-import Loading from "components/Loading"
 import CoinPagination from "components/Pagination/Pagination"
 import NewsOptions from "../newsOptions"
+import NewsPlaceholder from "components/News/placeholder"
 
 const NewsPage: React.FC = () => {
   const [newsData, setNewsData] = useState<TNews[]>()
@@ -19,15 +19,14 @@ const NewsPage: React.FC = () => {
   const [totalPages, setTotalPage] = useState(10)
   // const [category, setCategory] = useState("All")
 
-  const {
-    data: allNewsData,
-    isLoading,
-    isFetching,
-    refetch: refetchNews,
-  } = useQuery("all-news", () => getNews(pageNo, pageSize), {
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-  })
+  const { data: allNewsData, refetch: refetchNews } = useQuery(
+    "all-news",
+    () => getNews(pageNo, pageSize),
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  )
 
   //const
   useEffect(() => {
@@ -47,40 +46,43 @@ const NewsPage: React.FC = () => {
 
   return (
     <div className="container news-page">
-      <Loading loading={isLoading || isFetching}>
-        <div>
-          <br />
-          {newsData && <Headlines />}
+      <div>
+        <br />
+        {newsData && <Headlines />}
 
-          <Row>
-            <Col xs={12} sm={12} md={8} className="right-pane">
-              <h3>Latest news</h3>
-              <NewsOptions
-                setPageSize={setPageSize}
-                // setCategory={setCategory}
-              />
-              <Row xs={1} sm={1} md={1} lg={1} className="news-cards">
-                {newsData &&
-                  newsData?.map((eachNews: any, index: number) => (
-                    <div className="news-cards-each" key={index}>
-                      <LandscapeCards news={eachNews} />
-                    </div>
-                  ))}
+        <Row>
+          <Col xs={12} sm={12} md={8} className="right-pane">
+            <h3>Latest news</h3>
+            <NewsOptions
+              setPageSize={setPageSize}
+              // setCategory={setCategory}
+            />
+            <Row xs={1} sm={1} md={1} lg={1} className="news-cards">
+              {newsData ? (
+                newsData?.map((eachNews: any, index: number) => (
+                  <div className="news-cards-each" key={index}>
+                    <LandscapeCards news={eachNews} />
+                  </div>
+                ))
+              ) : (
+                <NewsPlaceholder count={5} />
+              )}
+              {newsData && (
                 <CoinPagination
                   pageNo={pageNo}
                   setPageNo={setPageNo}
                   totalPages={totalPages}
                 />
-              </Row>
-            </Col>
-            <Col xs={12} sm={12} md={4} className="left-pane">
-              <div className="news-brief-component">
-                <NewsBrief />
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </Loading>
+              )}
+            </Row>
+          </Col>
+          <Col xs={12} sm={12} md={4} className="left-pane">
+            <div className="news-brief-component">
+              <NewsBrief />
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
